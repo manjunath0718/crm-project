928d1e5d62ecb7942860a75e17b05b3d89c181d6
V Manjunath <manjunath0718@users.noreply.github.com>
2025-07-03 12:38:05 +0530

    DB

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/JUN25')
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://vmanjunathreddy07:ngGRLr3MraQL24cK@cluster0.jtiaasv.mongodb.net/')
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log('MongoDB Connection Error:', err));
 
